f003a91368519ea7e3e657512dbb445e61a5b297
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-21 00:03:08 +0800

    Add TopmoreKit solution.

---
+++ BuildAllTargets.cmd
@@ -0,0 +1,16 @@
+@setlocal
+@echo off
+
+rem Change to the current folder.
+cd "%~dp0"
+
+rem Remove the output folder for a fresh compile.
+rd /s /q Output
+
+rem Initialize Visual Studio environment
+call "%~dp0Mile.Project.Windows\InitializeVisualStudioEnvironment.cmd"
+
+rem Build all targets
+MSBuild -m BuildAllTargets.proj
+
+@endlocal
