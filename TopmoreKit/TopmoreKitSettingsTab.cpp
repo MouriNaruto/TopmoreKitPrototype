c090e4e043ca1702ecae207f7bbf1038a25a51b2
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-02-02 17:42:55 +0800

    Implement the website and support button in the settings tab.

--- TopmoreKit/TopmoreKitSettingsTab.cpp
+++ TopmoreKit/TopmoreKitSettingsTab.cpp
@@ -3,8 +3,27 @@
 TopmoreKitSettingsTab::TopmoreKitSettingsTab(QWidget *parent)
 	: QWidget(parent)
 {
-	ui.setupUi(this);
+    this->ui.setupUi(this);
+
+    this->connect(
+        this->ui.WebsitePushButton,
+        &QPushButton::clicked,
+        this,
+        []
+    {
+        ::OpenUriWithDefaultWebBrowser(L"https://www.topmore.com.tw");
+    });
+
+    this->connect(
+        this->ui.SupportPushButton,
+        &QPushButton::clicked,
+        this,
+        []
+    {
+        ::OpenUriWithDefaultWebBrowser(L"https://www.topmore.com.tw/edcontent_d.php?tb=3");
+    });
 }
 
 TopmoreKitSettingsTab::~TopmoreKitSettingsTab()
-{}
+{
+}
