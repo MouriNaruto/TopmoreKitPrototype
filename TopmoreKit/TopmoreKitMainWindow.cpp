63d50eaa345edab2222e48556b1159d4930f7299
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-02-02 12:03:57 +0800

    Improve the feature tab layout.

--- TopmoreKit/TopmoreKitMainWindow.cpp
+++ TopmoreKit/TopmoreKitMainWindow.cpp
@@ -1,9 +1,13 @@
 ﻿#include "TopmoreKitMainWindow.h"
 
+#include "TopmoreKitCustomTabStyle.h"
+
 TopmoreKitMainWindow::TopmoreKitMainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+    ui.FeatureTabWidget->tabBar()->setStyle(
+        new TopmoreKitCustomTabStyle());
 }
 
 TopmoreKitMainWindow::~TopmoreKitMainWindow()
